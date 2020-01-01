68b64a94f1b171852ffbe62998b1d1b46ecb0d06
Hiroshi Nakahara <hiro116s@gmail.com>
2020-01-02 07:05:39 +0900

    Create initial page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function BoardCreate(props) {
+  const [isFocused, setIsFocused] = useState(false);
+  const [boardName, setBoardName] = useState("");
+
+  function clearState() {
+    setBoardName("");
+    setIsFocused(false);
+  }
+
+  function handleSubmit(event) {
+    props.onBoardAdded(boardName);
+    clearState();
+    event.preventDefault();
+  }
+
+  if (isFocused) {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={boardName} onChange={(e) => setBoardName(e.target.value)}/>
+          </label>
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div onClick={() => setIsFocused(true)}>
+        Create board
+      </div>
+    );
+  }
+}
+
+function BoardList(props) {
+  return(
+    <div>
+      <ul className="BoardList">
+        {props.boardList.map((board) => 
+          <li>{board}</li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
 function App() {
+  const [boardList, setBoardList] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <BoardCreate onBoardAdded={newBoard => setBoardList([...boardList, newBoard])}/>
+      <BoardList boardList={boardList}/>
     </div>
   );
 }
