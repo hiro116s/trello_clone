2ff5a0a7e93a1efe2818a2cfafe2fa2ae8c864c5
Hiroshi Nakahara <hiro116s@gmail.com>
2020-01-13 14:23:48 +0900

    Add validation of create boart

--- src/home/BoardCreate.js
+++ src/home/BoardCreate.js
@@ -3,29 +3,38 @@ import React, { useState } from 'react';
 function BoardCreate(props) {
   const [counter, setCounter] = useState(props.currentCounter);
   const [isFocused, setIsFocused] = useState(false);
+  const [isInvalidInput, setIsInvalidInput] = useState(false);
   const [boardName, setBoardName] = useState("");
 
   function clearState() {
     setBoardName("");
     setIsFocused(false);
+    setIsInvalidInput(false);
   }
 
   function handleSubmit(event) {
-    props.onBoardAdded(createBoard(counter, boardName));
-    setCounter(counter + 1);
-    clearState();
+    if (boardName === '') {
+      setIsInvalidInput(true);
+    } else {
+      props.onBoardAdded(createBoard(counter, boardName));
+      setCounter(counter + 1);
+      clearState();
+    }
     event.preventDefault();
   }
 
   return (
     <div className="CreateBoard">
-      <div className="CreateBoard-button" onClick={() => setIsFocused(!isFocused)}>
+      <div className="CreateBoard-button" onClick={() => setIsFocused(true)}>
         <h3>Create board</h3>
       </div>
-      {isFocused && <form onSubmit={handleSubmit} onReset={() => setIsFocused(!isFocused)} className="CreateBoard-form">
+      {isFocused && <form onSubmit={handleSubmit} onReset={() => clearState()} className="CreateBoard-form">
         <label>
           What shall we call the board? <br />
           <input type="text" className='CreateBoard-input' name='board_name' value={boardName} onChange={(e) => setBoardName(e.target.value)} />
+          <p className='CreateBoard-error' aria-live='polite'>
+            {isInvalidInput && 'Oops! Looks like you forgot the name!'}
+          </p>
         </label>
         <br />
         <input type="reset" value="Cancel"></input>
